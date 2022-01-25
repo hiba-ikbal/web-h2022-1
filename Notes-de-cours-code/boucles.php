27ca0d812c94e0df0f7c90154e6c34eac758a54d
hiba-ikbal <65920885+hiba-ikbal@users.noreply.github.com>
2022-01-25 11:20:24 -0500

    premieres boucles

--- Notes-de-cours-code/boucles.php
+++ Notes-de-cours-code/boucles.php
@@ -10,6 +10,7 @@
     <h1> Champs radio</h1>
 <?php
     // premiere boucle
+    //  A NE PAS FAIRE 
     for ($i = 0; $i<10 ; $i++){
         echo "<div>";
         echo "<input type='radio' >" . $i;
@@ -17,7 +18,53 @@
         echo "<input type='radio' value=' $i'>";
         echo "</div>";
     }
-    //  A NE PAS FAIRE 
+    // meilleur facon de faire
+    for ($i = 0; $i<10 ; $i++){
+        
     ?>
+    <!-- pour mettre du code html dns du php -->
+      <div>
+          
+          <input type="radio" name = "<?= $i?>"> <?= $i?>
+      </div>
+     <?php
+    }
+
+    $j = 10;
+    while ($j>0){
+        $j--;
+        echo $j;
+        // NE PAS OUBLIER DE FAIRE J-- SINN BOUCLE INFINIE 
+    }
+
+    // Premier tableau
+     $tab = []; 
+    //  ou écrire array();
+    $tab[] = "Hello";
+    // Si les crochets sont collés sur la variable,c'est un push
+    // comme si on veut dire a l indice 0 c est pas prevu donc on le mets a la fin?
+    // pareil que array_push en js
+    $tab[] = "World";
+
+    //Premier foreach()
+
+    foreach($tab as $element){
+        ?>
+        <div>
+            <?= $element ?>
+        </div>
+        <?php
+    }
+    
+    // Des fois on a besoin que de echo de la tab 
+    //  si je fs echo $tab; ca dit que c'est un tableau et qu'il ne paut pas faire un echo d'un array 
+    // dans ce cas on ft var_dump -> plus pratique pour les structures complexes
+    // Ca dit et la valeur et le type de la var
+
+    var_dump($tab);
+
+    
+   
+   ?>
 </body>
 </html>
